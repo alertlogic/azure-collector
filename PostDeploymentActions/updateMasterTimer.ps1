bfcf42b2a4acc272c6ac2db3376e749ca29185e0
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2017-09-25 12:27:51 +0100

    Use seconds value for master trigger schedule.

--- PostDeploymentActions/updateMasterTimer.ps1
+++ PostDeploymentActions/updateMasterTimer.ps1
@@ -1,5 +1,7 @@
-$min = (Get-Date).Minute % 15 + 1
-$new_schedule = "0 $min-59/15 * * * *"
+$date = Get-Date
+$min = $date.Minute % 15 + 1
+$sec = $date.Second
+$new_schedule = "$sec $min-59/15 * * * *"
 Write-Output "Updating Master timer trigger with ($new_schedule)."
 $master_function = Get-Content '..\\wwwroot\\Master\\function.json' -raw | ConvertFrom-Json
 $master_function.bindings | % {if($_.name -eq 'AlertlogicMasterTimer'){$_.schedule=$new_schedule}}
